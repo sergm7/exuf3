e239beacdc70cf578a10bbaeba31b8c24813ee94
sergm7 <sergio_240493@hotmail.com>
2015-03-10 21:18:49 +0100

    rdgrefdg

--- exuf3/index.php
+++ exuf3/index.php
@@ -1,51 +1,35 @@
 
 <?php
 	ini_set('display_errors','On');
-	spl_autoload_register(function($class){
-		require 'lib/'.$class.'.php';
-	});
+    session_start();
+    include('db_connect.php');
+?> 
 
-	require 'lib/mpdo.php';
-	include  'lib/config.php';
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+</head>
+<body>
 
-	class Index{
-		static $db;
-		static $config;
+	<?php
+	    if(isset($_SESSION['user'])) {
 
-		static function route(){
-			$str_accio=$_SERVER['REQUEST_URI'];
-			$ar_accio=array_filter(explode('/',trim($str_accio)));
+	        echo "Bienvenido" <?=$_SESSION['user']?>
+	        <br />
+	        <a href="end_session.php">Salir</a>
+	<?php
+	    }else {
+	?>
+			<a href="login.php">Login</a>
+	<?php
+		if($_SESSION['user']) == "admin"{ ?>
 
-			return array_pop($ar_accio);
+			<a href="register.php">Registrar usuarios</a> <br><hr><hr> 
+	<?php
 		}
-
-		static function start($conf){
-			//carregar DB
-			self::$config=$conf;
-			
-			$db=mpdo::getInstance(self::$config);
-
-			$accio=self::route();
-			switch ($accio){
-				case 'insert':
-					echo 'insert';
-					//vista de inserció de registre
-					break;
-				case 'login':
-					
-					$v=new vlogin();//vista de login i entrada de registres
-					break;
-
-				case 'list':
-					echo 'list';
-					//llistar usuaris
-					break;
-				default: echo 'operacio incorrecta';
-			}
-
-		}	
-	}
-	
-	Index::start($conf);
-	
-
+	    }
+	?> 
+</body>
+</html>
